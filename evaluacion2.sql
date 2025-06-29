dd118c68ceb95686baa6c3276f1c040192f6d762
Andreina Teixeira <teixeirandreina@gmail.com>
2025-06-29 10:56:28 +0200

    missing exercise 14

--- evaluacion2.sql
+++ evaluacion2.sql
@@ -14,20 +14,20 @@ FROM film; -- compared with SELECT title FROM FILM; there were no duplicates
 
 /*EJERCICIO 2. Muestra los nombres de todas las películas que tengan una clasificación de "PG-13".*/
 
-SELECT DISTINCT(title)
+SELECT title
 FROM film
 WHERE rating = "PG-13";
 
 /*EJERCICIO 3. Encuentra el título y la descripción de todas las películas que contengan la palabra "amazing" en su
 descripción*/
 
-SELECT DISTINCT(title), "description" -- escape the word, it appears as a reserved word in sql
+SELECT title, "description" -- escape the word, it appears as a reserved word in sql
 FROM film
 WHERE title LIKE '%amazing%'; -- looking for "amazing" in any position of the title. Exported SELECT title FROM film; there are no matches.
 
 /*EJERCICIO 4. Encuentra el título de todas las películas que tengan una duración mayor a 120 minutos*/
 
-SELECT DISTINCT(title)
+SELECT title
 FROM film
 WHERE length >120;
 
@@ -44,7 +44,7 @@ WHERE last_name LIKE 'GIBSON';
 
 /*EJERCICIO 7. Encuentra los nombres de los actores que tengan un actor_id entre 10 y 20.*/
 
-SELECT CONCAT(first_name, " ", last_name)
+SELECT CONCAT(first_name, " ", last_name) AS actor_full_name
 FROM actor
 WHERE actor_id BETWEEN 10 AND 20;
 
@@ -102,7 +102,8 @@ WHERE film.title = "Indian Love";
 
 SELECT title
 FROM film
-WHERE "description" REGEXP ("^$DOG&" OR "^$CAT&"); -- INCOMPLETOOOO, NO FUNCIONA!!!!!!!!!!!!!
+WHERE "description" LIKE "% DOG%" OR "description" LIKE "% CAT%" -- INCOMPLETOOOO, NO FUNCIONA!!!!!!!!!!!!!
+AND "description" LIKE "%DOG %" OR "description" LIKE "%CAT %";
 
 /*EJERCICIO 15. Encuentra el título de todas las películas que fueron lanzadas entre el año 2005 y 2010.*/
 
@@ -120,4 +121,39 @@ WHERE category.name = "Family";
 
 /*EJERCICIO 17. Encuentra el título de todas las películas que son "R" y tienen una duración mayor a 2 horas en la tabla
 film.*/
+
+SELECT DISTINCT(title)
+FROM film
+WHERE rating = "R" AND length >120;
+
+/*18. Muestra el nombre y apellido de los actores que aparecen en más de 10 películas.*/
+
+
+/*19. Hay algún actor o actriz que no apareca en ninguna película en la tabla film_actor.*/
+
+
+/*20. Encuentra las categorías de películas que tienen un promedio de duración superior a 120 minutos y
+muestra el nombre de la categoría junto con el promedio de duración.*/
+
+
+/*21. Encuentra los actores que han actuado en al menos 5 películas y muestra el nombre del actor junto con
+la cantidad de películas en las que han actuado.*/
+
+
+/*22. Encuentra el título de todas las películas que fueron alquiladas por más de 5 días. Utiliza una
+subconsulta para encontrar los rental_ids con una duración superior a 5 días y luego selecciona las
+películas correspondientes.*/
+
+
+/*23. Encuentra el nombre y apellido de los actores que no han actuado en ninguna película de la categoría
+"Horror". Utiliza una subconsulta para encontrar los actores que han actuado en películas de la
+categoría "Horror" y luego exclúyelos de la lista de actores.*/
+
+
+/*24. Encuentra el título de las películas que son comedias y tienen una duración mayor a 180 minutos en la
+tabla film.*/
+
+
+/*25. Encuentra todos los actores que han actuado juntos en al menos una película. La consulta debe mostrar
+el nombre y apellido de los actores y el número de películas en las que han actuado juntos.*/
 
